3fcb7852812976babf9a34be1ff1af48fe9af43b
Mariba <jackmariba.jk@gmail.com>
2023-02-17 03:36:27 -0800

    0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - single numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; ++i)
+        {
+                putchar('0' + i);
+        }
+        putchar('\n');
+        return (0);
+}
