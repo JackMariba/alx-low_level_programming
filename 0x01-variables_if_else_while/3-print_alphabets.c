eb2b9e59d334bd33584d83d1582f06486422127e
Mariba <jackmariba.jk@gmail.com>
2023-02-17 02:48:54 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+        int i;
+        char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+
+        for (i = 0; i < 26; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n);
+        return (0);
+}
