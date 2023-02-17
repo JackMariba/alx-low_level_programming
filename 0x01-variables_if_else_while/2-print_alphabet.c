121670457d4e6baddab664b03382377b1a459a98
Mariba <jackmariba.jk@gmail.com>
2023-02-17 03:02:14 -0800

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 	int i;
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 26; ++i)
 	{
 		putchar(alp[i]);
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
