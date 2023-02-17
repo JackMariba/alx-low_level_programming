fcccd7425689126d3c3be3f42ae3f56a90f7de12
Mariba <jackmariba.jk@gmail.com>
2023-02-17 03:22:41 -0800

    0 to 9 using putchar fun

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - single numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i<10; ++i)
+	{
+		putchar(i + '0');
+	}
+	putchari('\n');
+	return (0);
