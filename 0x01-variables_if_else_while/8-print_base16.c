4141d56cd829b79f1ebaae76a91aa751f75350a5
Mariba <jackmariba.jk@gmail.com>
2023-02-17 05:01:47 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char hex[16] = "0123456789abcdef"
+	
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex[i]);
+	}
+	putchar('\n');
+	return 0;
+}
